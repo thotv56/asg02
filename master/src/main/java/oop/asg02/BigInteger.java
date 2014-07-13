fed8a8a647eae1905154745ecb3b43adbab8ff02
thotv56 <devtran.apple@gmail.com>
2014-07-14 00:13:20 +0700

    Bai asg02

---
+++ master/src/main/java/oop/asg02/BigInteger.java
@@ -0,0 +1,33 @@
+package oop.asg02;
+
+public class BigInteger
+{
+    public BigInteger(int init) {
+        
+    }
+
+    public BigInteger(String init) {
+        
+    }
+
+    public String toString() {
+        
+        return "";
+    }
+
+    public boolean equals(Object other) {
+        return toString().equals(other.toString());
+    }
+
+    public long toLong() {
+        return 0;  
+    }
+
+    public BigInteger add(BigInteger other) {
+        return null; 
+    }
+
+    public BigInteger subtract(BigInteger other) {
+        return null;  
+    }
+}
